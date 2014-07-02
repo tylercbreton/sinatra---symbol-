75d56823f5ee6e347efff59ca4ea6c716ea682b9
tylercbreton <tcbreton@gmail.com>
2014-07-01 20:43:11 -0700

    added views for each outcome

--- dice.rb
+++ dice.rb
@@ -5,10 +5,10 @@ get '/' do
   @opp_roll = rand(1..6)
 
   if @my_roll - @opp_roll < 0
-    erb :win #@ symbol is used to create a link btwn sinatra and the view
+    erb :lose #@ symbol is used to create a link btwn sinatra and the view
   elsif @my_roll - @opp_roll == 0
-    "You rolled a #{@my_roll} <br> Your opponent rolled a #{@opp_roll} <br> You tied"
-  else
-    "You rolled a #{@my_roll} <br> Your opponent rolled a #{@opp_roll} <br> You tied" 
+    erb :tied
+  else 
+    erb :win
   end 
 end 
